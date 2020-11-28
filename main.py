40bf2d386db0a10dc1609477d443e77b29486b6d
Andrej <andrej.janda@mail.utoronto.ca>
2020-11-28 12:00:49 -0500

    Updates main experiements

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from experiment_manager import (
     ExperimentParams, 
     ExperimentManager,
-    run_experiments
+    run_experiments,
+    compare_experiments
 )
 
-# experiments_to_run = ["blob_randomsampler", "blob_cvxsampler", "blob_cvxsampler_no_conf", "blob_cvxsampler_perf_conf"]
-experiments_to_run = ["blob_optimalsampler"]
-run_experiments(experiments_to_run)
+experiments_to_compare = [
+    "blob_cvxsampler_argmax",
+    "blob_cvxsampler_spectral"
+]
+compare_experiments(experiments_to_compare, "blob_cvx_clustering.jpeg")
+
+experiments_to_compare = [
+    "blob_randomsampler", 
+    "blob_cvxsampler", 
+    "blob_cvxsampler_no_conf", 
+    "blob_cvxsampler_perf_conf", 
+    "blob_optimal_sampler", 
+    "blob_cvxsampler_optimal_diversity"
+]
+compare_experiments(experiments_to_compare, "blob.jpeg")
 
