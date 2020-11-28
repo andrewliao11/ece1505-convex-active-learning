330e7fada525fa8bb2beefd964115c280ad15449
Andrej <andrej.janda@mail.utoronto.ca>
2020-11-28 18:32:42 -0500

    Updates main code to run experiments and generate comparison plots for the report

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ experiments_to_compare = [
 compare_experiments(experiments_to_compare, "blob_cvx_clustering.jpeg")
 
 experiments_to_compare = [
+    "moon_cvxsampler_argmax",
+    "moon_cvxsampler_spectral"
+]
+compare_experiments(experiments_to_compare, "moon_cvx_clustering.jpeg")
+
+experiments_to_compare = [
     "blob_randomsampler", 
+    "blob_cvxsampler", 
+]
+compare_experiments(experiments_to_compare, "blob_cvx_vs_random.jpeg")
+
+experiments_to_compare = [
     "blob_cvxsampler", 
     "blob_cvxsampler_no_conf", 
     "blob_cvxsampler_perf_conf", 
-    "blob_optimal_sampler", 
-    "blob_cvxsampler_optimal_diversity"
+    "blob_cvxsampler_optimal_diversity",
+    "blob_optimalsampler",
 ]
-compare_experiments(experiments_to_compare, "blob.jpeg")
+compare_experiments(experiments_to_compare, "blob_cvx.jpeg")
 
